c3ee793563b315044e85c169fcdab700e65322c6
Brock Boland <brock.boland@bestbuy.com>
2021-12-09 21:04:44 -0800

    Day 6 part 2

--- 2021/06/index.js
+++ 2021/06/index.js
@@ -1,30 +1,57 @@
 import { fileContents } from "advent-of-code-2021-a1329730-utils";
 
-// let fullInput = fileContents("./input.txt");
+let fullInput = fileContents("./input.txt");
 let sampleInput = fileContents("./sampleInput.txt");
+
+const maxFishDayCount = 8;
 
 function populationSize(input, days) {
     var school = input[0].split(",").map(n => parseInt(n));
+    let fishPerDay = listToPopulationCount(school);
+
     for(let d = 0; d < days; d++){
-        let startingPopulation = school.length;
-        console.log(`On day ${d} we have ${startingPopulation} fish`);
-        for(let f = 0; f < startingPopulation; f++) {
-            if (school[f] == 0) {
-                school[f] = 6;
-                school.push(8);
-            } else {
-                school[f]--;
-            }
+        let newFishToCreate = fishPerDay[0];
+        let fishForTomorrow = {};
+        for(let i = 1; i <= maxFishDayCount; i++) {
+            fishForTomorrow[i - 1] = fishPerDay[i];
         }
+        // Create the new babies
+        fishForTomorrow[maxFishDayCount] = newFishToCreate;
+        // Reset the new moms
+        fishForTomorrow[6] += newFishToCreate;
+
+        fishPerDay = fishForTomorrow;
     }
-    return school.length;
+
+    return totalFishCount(fishPerDay);
+}
+
+function listToPopulationCount(list) {
+    let fishPerDay = {};
+    for(let i = 0; i <= maxFishDayCount; i++) {
+        fishPerDay[i] = 0;
+    }
+
+    for(let f in list) {
+        let dayCount = list[f];
+        fishPerDay[dayCount]++;
+    }
+    return fishPerDay;
+}
+
+function totalFishCount(fishPerDay) {
+    let sum = 0;
+    for(let i = 0; i <= maxFishDayCount; i++) {
+        sum += fishPerDay[i];
+    }
+    return sum;
 }
 
 
 // PART 1
-// let population = populationSize(fullInput, 80);
+// let population = populationSize(sampleInput, 80);
 // console.log("After 80 days", population);
 
 // PART 2
-let bigPopulation = populationSize(sampleInput, 200);
+let bigPopulation = populationSize(fullInput, 256);
 console.log("After 256 days", bigPopulation);
