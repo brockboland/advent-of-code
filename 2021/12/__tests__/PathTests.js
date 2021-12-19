3700ad4cc215d0f34f21ec93b13ca26f9b8d693b
Brock Boland <brock.boland@bestbuy.com>
2021-12-18 20:12:40 -0600

    Figured out unit tests

--- 2021/12/__tests__/PathTests.js
+++ 2021/12/__tests__/PathTests.js
@@ -1,20 +1,31 @@
 import { fileContents } from "advent-of-code-2021-a1329730-utils";
 import { firstChallenge, secondChallenge } from "../index";
 
-test("Cave with 10 paths", () => {
-  let input = fileContents("./mocks/sample10.txt");
-  let result = firstChallenge(input);
-  expect(result).toEqual(10);
+describe("Part 1", ()=> {
+
+  test("Cave with 10 paths", () => {
+    let input = fileContents(mockFilePath("sample10.txt"));
+    expect(input.length).toEqual(7);
+    let result = firstChallenge(input);
+    expect(result).toEqual(10);
+  });
+
+  test("Cave with 19 paths", () => {
+    let input = fileContents(mockFilePath("sample19.txt"));
+    expect(input.length).toEqual(10);
+    let result = firstChallenge(input);
+    expect(result).toEqual(19);
+  });
+
+  test("Cave with 226 paths", () => {
+    let input = fileContents(mockFilePath("sample226.txt"));
+    expect(input.length).toEqual(18);
+    let result = firstChallenge(input);
+    expect(result).toEqual(226);
+  });
 });
 
-test("Cave with 19 paths", () => {
-  let input = fileContents("./mocks/sample19.txt");
-  let result = firstChallenge(input);
-  expect(result).toEqual(19);
-});
 
-test("Cave with 226 paths", () => {
-  let input = fileContents("./mocks/sample226.txt");
-  let result = firstChallenge(input);
-  expect(result).toEqual(226);
-});
+const mockFilePath = (filename) => { 
+  return `${__dirname}/sampleInputs/${filename}` 
+}
--- 2021/12/index.js
+++ 2021/12/index.js
@@ -1,20 +1,7 @@
-import { fileContents } from "advent-of-code-2021-a1329730-utils";
-
-let fullInput = fileContents("./input.txt");
-let sampleInput = fileContents("./sampleInput.txt");
-
 export const firstChallenge = (input) => {
-    
+    return 4;   
 }
 
 export const secondChallenge = (input) => {
-   
+    return 8;
 }
-
-// PART 1
-let part1 = firstChallenge(sampleInput);
-console.log("First challenge: ", part1);
-
-// PART 2
-let part2 = secondChallenge(sampleInput);
-console.log("Second challenge: ", part2);
