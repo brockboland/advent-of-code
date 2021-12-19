c6d2301d39e0364b9eb1a59825e4d18930485f22
Brock Boland <brock.boland@bestbuy.com>
2021-12-18 22:37:02 -0600

    Getting valid paths

--- 2021/12/__tests__/PathTests.js
+++ 2021/12/__tests__/PathTests.js
@@ -1,28 +1,60 @@
 import { fileContents } from "advent-of-code-2021-a1329730-utils";
-import { firstChallenge, secondChallenge } from "../index";
+import { firstChallenge, processCaveSystem, isBigCave, validPathsThrough, secondChallenge } from "../index";
 
 describe("Part 1", ()=> {
+  test("Cave validation", () => {
+    expect(isBigCave("start")).toBe(false);
+    expect(isBigCave("end")).toBe(false);
+    expect(isBigCave("a")).toBe(false);
+    expect(isBigCave("b")).toBe(false);
+    expect(isBigCave("c")).toBe(false);
+    expect(isBigCave("A")).toBe(true);
+    expect(isBigCave("B")).toBe(true);
+    expect(isBigCave("OKTHEN")).toBe(true);
+  });
 
-  test("Cave with 10 paths", () => {
+  test("Valid path structure for a small cave system", () => {
     let input = fileContents(mockFilePath("sample10.txt"));
-    expect(input.length).toEqual(7);
-    let result = firstChallenge(input);
-    expect(result).toEqual(10);
-  });
+    let caves = processCaveSystem(input);
+    expect(caves.A).toEqual(["start", "c", "b", "end"]);
+    expect(caves.b).toEqual(["start", "A", "d", "end"]);
+    expect(caves.start).toEqual(["A", "b"]);
+    expect(caves.end).toEqual(["A", "b"]);
+  })
+  
+  test("Valid paths to end", () => {
+    let input = fileContents(mockFilePath("sample10.txt"));
+    let caves = processCaveSystem(input);
+    let paths = validPathsThrough(caves, "b", []);
 
-  test("Cave with 19 paths", () => {
-    let input = fileContents(mockFilePath("sample19.txt"));
-    expect(input.length).toEqual(10);
-    let result = firstChallenge(input);
-    expect(result).toEqual(19);
-  });
+    console.log("Valid paths", paths);
 
-  test("Cave with 226 paths", () => {
-    let input = fileContents(mockFilePath("sample226.txt"));
-    expect(input.length).toEqual(18);
-    let result = firstChallenge(input);
-    expect(result).toEqual(226);
-  });
+    expect(paths.length).toEqual(3);
+    expect(paths[0]).toEqual(["A", "c", "A", "end"])
+    expect(paths[1]).toEqual(["A", "end"])
+    expect(paths[2]).toEqual(["end"])
+  })
+
+  // test("Cave with 10 paths", () => {
+  //   let input = fileContents(mockFilePath("sample10.txt"));
+  //   expect(input.length).toEqual(7);
+  //   let result = firstChallenge(input);
+  //   expect(result).toEqual(10);
+  // });
+
+  // test("Cave with 19 paths", () => {
+  //   let input = fileContents(mockFilePath("sample19.txt"));
+  //   expect(input.length).toEqual(10);
+  //   let result = firstChallenge(input);
+  //   expect(result).toEqual(19);
+  // });
+
+  // test("Cave with 226 paths", () => {
+  //   let input = fileContents(mockFilePath("sample226.txt"));
+  //   expect(input.length).toEqual(18);
+  //   let result = firstChallenge(input);
+  //   expect(result).toEqual(226);
+  // });
 });
 
 
--- 2021/12/index.js
+++ 2021/12/index.js
@@ -1,6 +1,81 @@
 export const firstChallenge = (input) => {
-    return 4;   
+    let caves = processCaveSystem(input);
+    let paths = validPathsThrough(caves);
+    console.log("Valid paths:", paths);
+    return paths.length;
 }
+
+export const processCaveSystem = (input) => {
+    let caves = {};
+
+    const addPath = (start, finish) => {
+        if (caves[start] != undefined) {
+            caves[start].push(finish);
+        } else {
+            caves[start] = [finish];
+        }
+    }
+
+    for (let i in input) {
+        let path = input[i];
+        let [a, b] = path.split("-");
+        addPath(a, b);
+        addPath(b, a);
+    }
+    return caves;
+}
+
+export const validPathsThrough = (caves, startingAt, pathSoFar) => {
+    const endNode = "end";
+
+    // pathSoFar can't be empty: at the very least, it includes the starting node
+    if (pathSoFar.length == 0) {
+        pathSoFar = [startingAt];
+    }
+
+    let branchingOptions = caves[startingAt];
+    if (branchingOptions.length == 0) {
+        return [];
+    } else if (branchingOptions.length == 1 && branchingOptions[0] == endNode) {
+        return [endNode];
+    } else {
+        let endingPathsFromHere = [];
+        for (let b in branchingOptions) {
+            let nextNode = branchingOptions[b];
+
+            // If the next node is an "end", stop there
+            if (nextNode == endNode) {
+                endingPathsFromHere.push([endNode]);
+            }
+            // If the next node is the start, you cna't go there
+            else if (nextNode == "start") {
+                continue;
+            }
+            // If the next one is a small cave we've already visited, don't go back
+            else if (!isBigCave(nextNode) && pathSoFar.includes(nextNode)) {
+                continue;
+            }
+            else {
+                let pathForNext = pathSoFar.concat(nextNode);
+                let pathsFromHere = validPathsThrough(caves, nextNode, pathForNext);
+                for(let p in pathsFromHere) {
+                    endingPathsFromHere.push([nextNode].concat(pathsFromHere[p]));
+                }
+            }
+        }
+        return endingPathsFromHere;
+    }
+}
+
+export const isBigCave = (name) => {
+    return name === name.toUpperCase();
+}
+
+
+
+
+
+
 
 export const secondChallenge = (input) => {
     return 8;
