107b41afafb842d49e8ee972ee11d89a7f46687b
Brock Boland <brock.boland@bestbuy.com>
2022-11-26 13:40:17 -0800

    Start Next.js project for 2022

---
+++ 2022/pages/day/1.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+export default function Day1() {
+  return (
+    <div>
+      <Head>
+        <title>Day 1</title>
+      </Head>
+
+      <main>
+        <h1>Day 1</h1>
+      </main>
+    </div>
+  )
+}
