ddf271568ddda0b86cfe23f4b68d793a11bc708c
Brock Boland <brock.boland@bestbuy.com>
2022-12-04 21:41:54 -0800

    Day 5 solution

--- 2022/pages/day/5/index.js
+++ 2022/pages/day/5/index.js
@@ -52,9 +52,77 @@ export default function Day5({ sampleData, realData }) {
 	};
 
 	// BEGIN DAY 5 SPECIFIC LOGIC
+	finalDetails.part1SampleExpected = "CMZ";
+	finalDetails.part1RealExpected = "RFFFWBPNS";
+
 	// Part 1
+	const startingDetails = (data) => {
+		const [startingCrateArrangement, rawMoveInstructions] = data.split("\n\n");
+
+		let crateStacks = [];
+		startingCrateArrangement.split("\n").forEach(row => {
+			for (let i = 0; i < (row.length/4); i++) {
+				if (row[i*4] == "[") {
+					const crateName = row[i*4 + 1];
+					if (!crateStacks[i+1]) crateStacks[i+1] = [];
+					crateStacks[i+1].push(crateName);
+				}
+			}
+		});
+		const stacks = crateStacks.map(a => a.reverse());
+		
+		const moveInstructions = rawMoveInstructions.split("\n").map(l => {
+			return l.split(" ").map(i => parseInt(i)).filter(i => i > 0);
+		})
+		return {stacks, moveInstructions};
+	}
+
+	const executeMoves = (stacks, instructions) => {
+		let newStacks = [...stacks];
+		instructions.forEach(i => {
+			const [crateCount, moveFromStack, moveToStack] = i;
+			for (let c = 0; c < crateCount; c++) {
+				newStacks[moveToStack].push(newStacks[moveFromStack].pop());
+			}
+		})
+		return newStacks;
+	}
+
+	const part1FullProcess = (data) => {
+		const {stacks, moveInstructions} = startingDetails(data);
+		const finish = executeMoves(stacks, moveInstructions);
+		return finish.filter(a => !!a).map(a => a.at(-1)).join("");
+	}
+
+	finalDetails.part1SampleCalculated = part1FullProcess(sampleData);
+	finalDetails.part1RealCalculated = part1FullProcess(realData);
 
 	// Part 2
+	finalDetails.part2SampleExpected = "MCD";
+	finalDetails.part2RealExpected = "CQQBBJFCS";
+
+	const executeMovesInBulk = (stacks, instructions) => {
+		let newStacks = [...stacks];
+		instructions.forEach(i => {
+			const [crateCount, moveFromStack, moveToStack] = i;
+			let cratesToMove = [];
+			for (let c = 0; c < crateCount; c++) {
+				cratesToMove.push(newStacks[moveFromStack].pop());
+			}
+
+			cratesToMove.reverse().forEach(c => newStacks[moveToStack].push(c));
+		})
+		return newStacks;
+	}
+
+	const part2FullProcess = (data) => {
+		const {stacks, moveInstructions} = startingDetails(data);
+		const finish = executeMovesInBulk(stacks, moveInstructions);
+		return finish.filter(a => !!a).map(a => a.at(-1)).join("");
+	}
+
+	finalDetails.part2SampleCalculated = part2FullProcess(sampleData);
+	finalDetails.part2RealCalculated = part2FullProcess(realData);
 
 	// END DAY 5 SPECIFIC LOGIC
 
