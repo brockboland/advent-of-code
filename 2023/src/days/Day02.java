572719db66085dc609353bcf6e50770626602b45
Brock Boland <brock@brockboland.com>
2023-11-30 22:00:14 -0800

    Cleared out for day 2

--- 2023/src/days/Day02.java
+++ 2023/src/days/Day02.java
@@ -16,32 +16,11 @@ public class Day02 extends DayRunner {
     }
 
     public String part1(List<String> fileLines) {
-        Map<String, Integer> scoresMap = new HashMap<>();
-        // Opponent:
-        // A for Rock, B for Paper, and C for Scissors
-        // Self:
-        // X for Rock, Y for Paper, and Z for Scissors
-        scoresMap.put("A X", Integer.valueOf(4));
-        scoresMap.put("A Y", Integer.valueOf(8));
-        scoresMap.put("A Z", Integer.valueOf(3));
-        scoresMap.put("B X", Integer.valueOf(1));
-        scoresMap.put("B Y", Integer.valueOf(5));
-        scoresMap.put("B Z", Integer.valueOf(9));
-        scoresMap.put("C X", Integer.valueOf(7));
-        scoresMap.put("C Y", Integer.valueOf(2));
-        scoresMap.put("C Z", Integer.valueOf(6));
-
-        int score = 0;
-        for (String string : fileLines) {
-            int thisScore = scoresMap.get(string).intValue();
-            // System.out.println(String.format("Score for %s was %d", string, thisScore));
-            score += thisScore;
-        }
-        return Integer.toString(score);
+       return "TODO";
     }
 
     public String part2(List<String> fileLines) {
-        return "pretend this is good";
+        return "TODO";
     }
 
 }
