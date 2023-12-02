6dec368b381374446abde28ca7462d1b5ee6a9eb
Brock Boland <brock@brockboland.com>
2023-12-01 22:27:17 -0800

    Day 3 prep

---
+++ 2023/src/days/Day03.java
@@ -0,0 +1,24 @@
+package days;
+
+import java.util.List;
+
+import common.DayRunner;
+
+public class Day03 extends DayRunner {
+    public Day03() {
+        super(3);
+    }
+
+    public static void main(String[] args) {
+        new Day03().run();
+    }
+
+    public String part1(List<String> fileLines) {
+        return "TODO part 1";
+    }
+
+    public String part2(List<String> fileLines) {
+        return "TODO part 2";
+    }
+
+}
