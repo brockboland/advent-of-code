a9179e0d7be17798bd72ed832dbd92ab7910403d
Brock Boland <brock@brockboland.com>
2023-12-01 21:37:24 -0800

    Cleaned up a bit

--- 2023/src/days/Day02.java
+++ 2023/src/days/Day02.java
@@ -38,24 +38,18 @@ public class Day02 extends DayRunner {
 
             String[] reveals = x[1].split("; ");
 
-            revealLoop: for (String reveal : reveals) {
+            for (String reveal : reveals) {
                 String[] colorsInReveal = reveal.split(", ");
 
-                colorLoop: for (String colorDetails : colorsInReveal) {
+                for (String colorDetails : colorsInReveal) {
                     String color = colorDetails.split(" ")[1];
                     int cubes = Integer.valueOf(colorDetails.split(" ")[0]).intValue();
 
-                    if (color.equals("red")) {
-                        if (cubes > maxRed)
-                            continue gameLoop;
-                    } else if (color.equals("green")) {
-                        if (cubes > maxGreen)
-                            continue gameLoop;
-                    } else if (color.equals("blue")) {
-                        if (cubes > maxBlue)
-                            continue gameLoop;
-                    } else {
-                        System.err.println("FAILURE unexpected color: " + color);
+                    // If we have revealed too many of any color, skip to the next game
+                    if ((color.equals("red") && cubes > maxRed)
+                            || (color.equals("green") && cubes > maxGreen)
+                            || (color.equals("blue") && cubes > maxBlue)) {
+                        continue gameLoop;
                     }
                 }
             }
@@ -75,20 +69,17 @@ public class Day02 extends DayRunner {
         // is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively. Adding up
         // these five powers produces the sum 2286.
 
-        gameLoop: for (String gameInfo : fileLines) {
+        for (String gameInfo : fileLines) {
             int minRed = 0;
             int minGreen = 0;
             int minBlue = 0;
 
-            String[] x = gameInfo.split(": ");
-            int gameNumber = Integer.valueOf(x[0].replaceAll("[^0-9]", "")).intValue();
+            String[] reveals = gameInfo.split(": ")[1].split("; ");
 
-            String[] reveals = x[1].split("; ");
-
-            revealLoop: for (String reveal : reveals) {
+            for (String reveal : reveals) {
                 String[] colorsInReveal = reveal.split(", ");
 
-                colorLoop: for (String colorDetails : colorsInReveal) {
+                for (String colorDetails : colorsInReveal) {
                     String color = colorDetails.split(" ")[1];
                     int cubes = Integer.valueOf(colorDetails.split(" ")[0]).intValue();
 
@@ -105,8 +96,6 @@ public class Day02 extends DayRunner {
             }
 
             int gameScore = minRed * minBlue * minGreen;
-            // System.out.println(String.format("Game %d score: %d", gameNumber, gameScore));
-            // System.out.println(String.format("Red %d, Green %d, %d blue", minRed, minGreen, minBlue));
             sumOfPowers += gameScore;
         }
         return String.valueOf(sumOfPowers);
