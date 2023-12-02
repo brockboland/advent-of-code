668f086d364ab598bf658538fde3049918872974
Brock Boland <brock@brockboland.com>
2023-12-01 21:36:41 -0800

    Add colors to result output

--- 2023/src/common/DayRunner.java
+++ 2023/src/common/DayRunner.java
@@ -8,6 +8,16 @@ public abstract class DayRunner {
     public abstract String part1(List<String> fileLines);
     public abstract String part2(List<String> fileLines);
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
     public DayRunner() {
         this(0);
     }
@@ -19,7 +29,16 @@ public abstract class DayRunner {
     public void run() {
         String part1Result = part1(fileLines);
         String part2Result = part2(fileLines);
-        System.out.println(String.format("Part 1 result:%n%s", part1Result));
-        System.out.println(String.format("Part 2 result:%n%s", part2Result));
+
+        System.out.println(ANSI_RESET);
+        System.out.println(ANSI_GREEN + "Part 1 result:");
+        System.out.println(ANSI_BLUE + part1Result);
+
+        System.out.println(ANSI_RESET);
+
+        System.out.println(ANSI_GREEN + "Part 2 result:");
+        System.out.println(ANSI_BLUE + part2Result);
+
+        System.out.println(ANSI_RESET);
     }
 }
