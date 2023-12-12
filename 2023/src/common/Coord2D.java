33cb108f0c898620b73ef66cfbc61677f0a461fb
Brock Boland <brock@brockboland.com>
2023-12-11 20:49:56 -0800

    Started a grid helper

--- 2023/src/common/Coord2D.java
+++ 2023/src/common/Coord2D.java
@@ -1,10 +1,12 @@
 package common;
 
 public class Coord2D {
-    
+
     private final int x;
     private final int y;
 
+    // Grid assumes 0,0 is top left
+    // Going "down" results in a higher y value
     public Coord2D(int x, int y) {
         this.x = x;
         this.y = y;
@@ -16,6 +18,22 @@ public class Coord2D {
 
     public final int y() {
         return this.y;
+    }
+
+    public Coord2D up() {
+        return new Coord2D(x, y - 1);
+    }
+
+    public Coord2D down() {
+        return new Coord2D(x, y + 1);
+    }
+
+    public Coord2D left() {
+        return new Coord2D(x - 1, y);
+    }
+
+    public Coord2D right() {
+        return new Coord2D(x + 1, y);
     }
 
     // Check if the other coordinate is in the 8 spots around this one
@@ -30,10 +48,8 @@ public class Coord2D {
     }
 
     @Override
-        public String toString() {
-            return String.format("(%d, %d)", x, y);
-        }
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
 
 }
-
-
---
+++ 2023/src/common/Grid.java
@@ -0,0 +1,81 @@
+package common;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Grid {
+
+    private List<List<Character>> rows;
+
+    public Grid(List<String> input) {
+        rows = new ArrayList<>();
+
+        for (String s : input) {
+            List<Character> row = s.chars().mapToObj(i -> (char) i).map(c -> Character.valueOf(c))
+                    .collect(Collectors.toList());
+            rows.add(row);
+        }
+    }
+
+    public int columnCount() {
+        return rows.get(0).size();
+    }
+
+    public int rowCount() {
+        return rows.size();
+    }
+
+    // 0-indexed x and y
+    public Character get(int x, int y) {
+        return rows.get(y).get(x);
+    }
+
+    public Character get(Coord2D coord) {
+        return get(coord.x(), coord.y());
+    }
+
+    public boolean rowContains(int y, char c) {
+        for (Character character : rows.get(y)) {
+            if (character.charValue() == c) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean colContains(int x, char c) {
+        for (List<Character> row : rows) {
+            if (row.get(x).charValue() == c) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void insertRow(int newY, char fillWithChar) {
+        List<Character> newRow = new ArrayList<>();
+        for (int i = 0; i < columnCount(); i++) {
+            newRow.add(Character.valueOf(fillWithChar));
+        }
+        rows.add(newY, newRow);
+    }
+
+    public void insertColumn(int newX, char fillWithChar) {
+        for (int i = 0; i < rowCount(); i++) {
+            rows.get(i).add(newX, Character.valueOf(fillWithChar));
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder b = new StringBuilder();
+        for (List<Character> row : rows) {
+            for (Character character : row) {
+                b.append(character);
+            }
+            b.append(String.format("%n"));
+        }
+        return b.toString();
+    }
+}
