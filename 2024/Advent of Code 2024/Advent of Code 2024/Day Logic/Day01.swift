8187447490027daa4970ff263f4d1f27e6c29602
Brock Boland <brock@brockboland.com>
2024-11-26 19:50:57 -0800

    Finished Day 1 from last year as an example

--- 2024/Advent of Code 2024/Advent of Code 2024/Day Logic/Day01.swift
+++ 2024/Advent of Code 2024/Advent of Code 2024/Day Logic/Day01.swift
@@ -8,28 +8,51 @@
 import Foundation
 
 struct Day01 {
-    static func firstNum(sub: String) -> String.Element? {
+    private static let numberWords: [String: Character] = [
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+        "zero": "0"
+    ]
+    
+    static func firstNum(sub: String, useWords: Bool = false) -> Character? {
         if (sub.isEmpty) {
             return nil
-        }
-        if (sub.first?.isNumber == true) {
+        } else if (sub.first?.isNumber == true) {
             return sub.first
-        } else {
-            let nextString = sub[sub.index(sub.startIndex, offsetBy: 1)...]
-            return firstNum(sub: String(nextString));
+        } else if (useWords) {
+            for word in numberWords {
+                if (sub.hasPrefix(word.key)) {
+                    return word.value
+                }
+            }
         }
+        
+        let nextString = sub[sub.index(sub.startIndex, offsetBy: 1)...]
+        return firstNum(sub: String(nextString), useWords: useWords);
     }
     
-    static func lastNum(sub: String) -> String.Element? {
+    static func lastNum(sub: String, useWords: Bool = false) -> Character? {
         if (sub.isEmpty) {
             return nil
-        }
-        if (sub.last?.isNumber == true) {
+        } else if (sub.last?.isNumber == true) {
             return sub.last
-        } else {
-            let nextString = sub[..<sub.index(sub.endIndex, offsetBy: -1)]
-            return lastNum(sub: String(nextString));
+        } else if (useWords) {
+            for word in numberWords {
+                if (sub.hasSuffix(word.key)) {
+                    return word.value
+                }
+            }
         }
+        
+        let nextString = sub[..<sub.index(sub.endIndex, offsetBy: -1)]
+        return lastNum(sub: String(nextString), useWords: useWords);
     }
     
     struct Part1 {
@@ -75,28 +98,17 @@ struct Day01 {
             }
             return part2Logic(input: contents)
         }
-        
-        private static let numberWords = [
-            "one": 1,
-            "two": 2,
-            "three": 3,
-            "four": 4,
-            "five": 5,
-            "six": 6,
-            "seven": 7,
-            "eight": 8,
-            "nine": 9,
-            "zero": 0
-        ]
+    
         
         private static func part2Logic(input: [String]) -> String {
             
             var sum = 0
-            
             for line in input {
-                let f = firstNum(sub: line);
+                let twoDigit = (firstNum(sub: line, useWords: true)?.description ?? "") + (lastNum(sub: line, useWords: true)?.description ?? "")
+                if let d = Int(twoDigit) {
+                    sum += d
+                }
             }
-            
             return String(sum)
         }
         
