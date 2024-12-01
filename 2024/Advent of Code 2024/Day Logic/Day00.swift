6be3073378a7d76f3dd2710b34f4dab282abc9ed
Brock Boland <brock@brockboland.com>
2024-11-30 18:48:59 -0800

    Placeholder for each day

---
+++ 2024/Advent of Code 2024/Day Logic/Day00.swift
@@ -0,0 +1,55 @@
+//
+//  Day00.swift
+//  Advent of Code 2024
+//
+//  Created by Brock Boland on 11/26/24.
+//
+
+import Foundation
+
+struct Day00 {
+    
+    struct Part1 {
+        static func exampleInput() -> String {
+            guard let contents = InputReader.contentsOf(filename: "00-example1") else {
+                return "Couldn't read input"
+            }
+            return part1Logic(input: contents)
+        }
+        
+        static func realInput() -> String {
+            guard let contents = InputReader.contentsOf(filename: "00") else {
+                return "Couldn't read input"
+            }
+            return part1Logic(input: contents)
+        }
+        
+        private static func part1Logic(input: [String]) -> String {
+            return "Not implemented yet"
+        }
+    }
+    
+
+    
+    struct Part2 {
+        static func exampleInput() -> String {
+            guard let contents = InputReader.contentsOf(filename: "00-example2") else {
+                return "Couldn't read input"
+            }
+            return part2Logic(input: contents)
+        }
+        
+        static func realInput() -> String {
+            guard let contents = InputReader.contentsOf(filename: "00") else {
+                return "Couldn't read input"
+            }
+            return part2Logic(input: contents)
+        }
+    
+        
+        private static func part2Logic(input: [String]) -> String {
+            return "Not implemented yet"
+        }
+        
+    }
+}
