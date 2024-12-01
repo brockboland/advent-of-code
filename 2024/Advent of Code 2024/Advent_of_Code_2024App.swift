58f9d5eef67540db662904cee47d4e82bcae8ea0
Brock Boland <brock@brockboland.com>
2024-11-30 18:49:15 -0800

    Basic NavigationSplitView

--- 2024/Advent of Code 2024/Advent_of_Code_2024App.swift
+++ 2024/Advent of Code 2024/Advent_of_Code_2024App.swift
@@ -9,12 +9,36 @@ import SwiftUI
 
 @main
 struct Advent_of_Code_2024App: App {
+    
+    struct MenuItem: Identifiable, Hashable {
+        var id = UUID()
+        var dateNumber: Int
+    }
+    
+    func dateNumber(for id: MenuItem.ID) -> Int {
+        guard let menuItem = dateList.first(where: { $0.id == id }) else {
+            return 1
+        }
+
+        return menuItem.dateNumber
+    }
+    
+    @State private var selectedItemId: MenuItem.ID?
+    
+    var dateList: [MenuItem] = Array(0...25).map { MenuItem(dateNumber: $0) }
+    
     var body: some Scene {
         WindowGroup {
             NavigationSplitView {
-                CalendarView()
+                List(dateList, selection: $selectedItemId) { item in
+                    Text(String(item.dateNumber))
+                }
             } detail: {
-                DayDetail(date: 3)
+                if let selectedItemId {
+                    DayDetail(date: dateNumber(for: selectedItemId))
+                } else {
+                    Text("Please select a date")
+                }
             }
         }
     }
